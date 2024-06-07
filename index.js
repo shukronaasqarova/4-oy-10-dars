c1295b62588771a4cacf46127185d13baf3ca239
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-06-07 12:29:31 +0500

    end

---
+++ index.js
@@ -0,0 +1,209 @@
+// forEach
+
+// 1 masala
+
+// let arr = [1,2,3,4,5,6];
+// let sum = [];
+// let res = arr.forEach(function(value){
+//     sum.push(value**2);
+// });
+// console.log(sum);
+
+// 2 masala
+
+// let arr = [1,-2,3,4,-5,-6];
+// let res = [];
+// arr.forEach(function(value){
+//     if (value > 0){
+//         res.push(value);
+//     }
+// })
+// console.log(res);
+
+// 3 masala
+
+// let arr = [1,2,3,4,5,6];
+// let res = arr.forEach(function(value){
+//     console.log(value*2);
+// })
+
+// 4 masala
+
+// let arr = [1,2,3,4,5,6];
+// let sum = 0;
+// let res = arr.forEach(function(value){
+//     sum=sum+value;
+// });
+// console.log(sum);
+
+// filter metodi
+
+// 1 masala
+
+// let arr = [1,9,-8,8,-2];
+// let res = arr.filter(function(value){
+//     return value > 0;
+// })
+// console.log(res);
+
+// 2 masala
+
+// let arr = [1,9,8,2,4];
+// let res = arr.filter(function(value){
+//     return value % 2 == 0;
+// })
+// console.log(res);
+
+// 3 masala
+
+// let arr = ['noza','guli','zarnigor','nodir','xasan'];
+// let res = arr.filter(function (value) {
+//     return value.length < 5;
+// })
+// console.log(res);
+
+// 4 masala
+
+// let users = [
+//   { name: "baxtiyor", age: 83 },
+//   { name: "xasan", age: 23 },
+//   { name: "umida", age: 18 },
+//   { name: "noza", age: 40 },
+// ];
+
+//  map metodi
+
+// 1 masala
+
+// let arr = [1,2,3,4,5,6];
+// let res = arr.map(function(value){
+//     return value**2
+// })
+// console.log(res);
+
+// 2 masala
+
+// let arr = [1,2,3,4,5,6];
+// let res = arr.map(function(value){
+//     return value+5
+// })
+// console.log(res);
+
+
+// some/every
+
+// 1 masala
+
+// let arr = [1,-5,2,-8,5,1];
+// let res = arr.some(function(value){
+//     return value < 0;
+// })
+// console.log(res);
+
+// 2 masala
+
+// let arr = [1,-5,2,-8,5,1];
+// let res = arr.every(function(value){
+//     return value > 0;
+// })
+// console.log(res);
+
+// umumiy
+
+// 1 masala
+
+// let arr = [1,8,3,4,6];
+// let res = arr.includes(6);
+// console.log(res);
+
+// 2 masala
+
+// let arr = [1,8,6,3,4,6];
+// let res = arr.indexOf(6);
+// console.log(res);
+
+// 3 masala
+
+// let arr = [1,8,6,3,4,6];
+// let res = arr.lastIndexOf(6);
+// console.log(res);
+
+// 4 masala
+
+// let arr = [1,8,5,9,84,41];
+// let res = arr.find(function(value){
+//     return value > 10;
+// });
+
+// console.log(res);
+
+//  5 masala
+
+// let arr = [1,8,5,9,84,41];
+// let res = arr.findIndex(function(value){
+//     return value > 10;
+// });
+// console.log(res);
+
+// 6 masala
+
+// let arr = [1,8,5,9,84,42,25,41,2];
+// let res = arr.findLastIndex(function(value){
+//     return value > 10;
+// });
+// console.log(res);
+
+// 7 masala
+
+// let arr = [1,2,3,4,5,6];
+// let res = arr.reverse();
+// console.log(res);
+
+// 8 masala
+
+// let arr = [-1,-5,-6,1,5,2,9,3,4,7,8];
+// let res = arr.sort(function(a,b){
+//     return a + b;
+// });
+// console.log(res);
+
+// 9 masala
+
+// let arr = [-1,-5,-6,1,5,2,9,3,4,7,8];
+// let res = arr.sort(function(a,b){
+//     return a - b;
+// });
+// console.log(res);
+
+// 10 masala xatooo
+
+// let arr = [1,2,3,,4,5,6];
+// let res = 1;
+// arr.forEach[function(value){
+//     res +=value
+// }]
+// console.log(res);
+
+// 12 masala
+
+// let arr = [1,2,3,4,5,6];
+// let res = arr.map(function(value){
+//     return value**2
+// })
+// console.log(res);
+
+// 13 masala
+
+// let arr = [1,-5,2,-8,5,1];
+// let res = arr.some(function(value){
+//     return value < 0;
+// })
+// console.log(res);
+
+// 14 masala
+
+// let arr = [1,-5,2,-8,5,1];
+// let res = arr.every(function(value){
+//     return value > 0;
+// })
+// console.log(res);
